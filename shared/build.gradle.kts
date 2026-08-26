4c5eb51ff17e034f7f6e751a205c57c492e36379
Preetam Bhosle <preetb123@gmail.com>
2026-08-27 01:19:33 +0530

    upgraded parikshan version to 0.0.9

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.androidMultiplatformLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    id("io.github.aryapreetam.parikshan") version "0.0.8"
+    id("io.github.aryapreetam.parikshan") version "0.0.9"
 }
 
 kotlin {
